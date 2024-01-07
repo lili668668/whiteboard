a59e72d6693ae2845b6707f6d5b4df8d5c283e43
Tzu-Yin Hong <lili668668@gmail.com>
2024-01-07 15:53:37 +0800

    feat: add whiteboard

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ReactSketchCanvas } from 'react-sketch-canvas'
+
+function App() {
+  return (
+    <ReactSketchCanvas
+      width={`${window.innerWidth}px`}
+      height={`${window.innerHeight}px`}
+      strokeWidth={4}
+      strokeColor="red"
+    />
+  )
+}
+
+export default App
